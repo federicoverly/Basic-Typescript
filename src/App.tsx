e7d551a42c6906b7f42f8d6ad2b2953253dee4fc
Federico Verly <federicoverly@gmail.com>
2022-09-01 19:23:46 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { BasicTypes } from './typescript/BasicTypes'
-import { ObjectLiteral } from './typescript/ObjectLiteral'
-import { Functions } from './typescript/Functions';
-import { Count } from './components/Count';
-import { CountWithHook } from './components/CountWithHook';
-import { Login } from './components/Login';
-import { Users } from './components/Users';
-import { Formulaires } from './components/Formulaires';
+import React from "react";
+import { BasicTypes } from "./typescript/BasicTypes";
+import { ObjectLiteral } from "./typescript/ObjectLiteral";
+import { Functions } from "./typescript/Functions";
+import { Count } from "./components/Count";
+import { CountWithHook } from "./components/CountWithHook";
+import { Login } from "./components/Login";
+import { Users } from "./components/Users";
+import { Formulaires } from "./components/Formulaires";
 
 const App = () => {
   return (
-    <div className='mt-2'>
+    <div className="mt-2">
       <h1>Typescript Introduction</h1>
       <hr />
       <BasicTypes />
@@ -29,7 +29,7 @@ const App = () => {
       <hr />
       <Formulaires />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
